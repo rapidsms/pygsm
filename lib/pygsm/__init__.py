7a8cae06ff191328af6e385fdd6dd7193fd4905f
Tobias McNulty <tobias@caktusgroup.com>
2011-08-18 13:31:40 -0400

    Take 2: don't import pygsm during install process as it may break

--- lib/pygsm/__init__.py
+++ lib/pygsm/__init__.py
@@ -4,18 +4,3 @@
 
 from gsmmodem import GsmModem
 __doc__ = GsmModem.__doc__
-
-def get_version():
-    try:
-        # do we have a static version set from an install?
-        from __version__ import VERSION as version
-    except ImportError:
-        # if not, can we figure it out from the git tag?
-        import commands
-        try:
-            # see http://stackoverflow.com/questions/62264/#72874
-            version = commands.getoutput("git describe --tags --always")
-        except:
-            # otherwise, give up!
-            version = "unknown"
-    return version
--- setup.py
+++ setup.py
@@ -37,7 +37,23 @@ os.chdir(wd)
 # are tagged with a reasonably exact git version.
 #
 sys.path = ["lib"] + sys.path
-version = __import__("pygsm").get_version()
+
+def get_version():
+    try:
+        # do we have a static version set from an install?
+        from __version__ import VERSION as version
+    except ImportError:
+        # if not, can we figure it out from the git tag?
+        import commands
+        try:
+            # see http://stackoverflow.com/questions/62264/#72874
+            version = commands.getoutput("git describe --tags --always")
+        except:
+            # otherwise, give up!
+            version = "unknown"
+    return version
+
+version = get_version()
 
 class build_py (_build_py):
     def run (self):
