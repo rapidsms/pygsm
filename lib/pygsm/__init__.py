e23a8ef8e5b6f382f8f4912ec35266f8c4b426e9
Tobias McNulty <tobias@caktusgroup.com>
2011-08-18 13:25:18 -0400

    Revert "removing lines from __init__.py that cause import error for pytz during install"
    This reverts commit b22af8ca6f284ab5b758e26ffa00617e1e39bc49.

--- lib/pygsm/__init__.py
+++ lib/pygsm/__init__.py
@@ -2,6 +2,9 @@
 # vim: ai ts=4 sts=4 et sw=4i encoding=utf-8
 
 
+from gsmmodem import GsmModem
+__doc__ = GsmModem.__doc__
+
 def get_version():
     try:
         # do we have a static version set from an install?
